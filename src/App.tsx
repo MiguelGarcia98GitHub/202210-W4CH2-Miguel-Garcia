2b0da5b2ad8b0678f5e63111630811086c7dd6ae
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-25 23:14:22 +0200

    Fixed PersonalDataForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { PersonalDataForm } from "./components/formularios/PersonalDataForm";
 import "./App.css";
 import { useState } from "react";
-import { NextAndPrevButtons } from "./components/formularios/NextAndPrevButtons";
 
 function App() {
     const [currentForm, setCurrentForm] = useState(0);
     console.log(currentForm);
-    const [validForm1, setValidForm1] = useState(false);
 
     return (
         <div className="App">
