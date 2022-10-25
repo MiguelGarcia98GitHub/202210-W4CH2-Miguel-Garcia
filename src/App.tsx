171f900f3ab47ca3ff0c1001dde50e99067850c8
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-25 23:12:00 +0200

    Added PersonalDataForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { PersonalDataForm } from "./components/formularios/PersonalDataForm";
+import "./App.css";
+import { useState } from "react";
+import { NextAndPrevButtons } from "./components/formularios/NextAndPrevButtons";
 
 function App() {
-    return <div className="App">FORMS</div>;
+    const [currentForm, setCurrentForm] = useState(0);
+    console.log(currentForm);
+    const [validForm1, setValidForm1] = useState(false);
+
+    return (
+        <div className="App">
+            {currentForm === 0 ? (
+                <PersonalDataForm
+                    currentForm={currentForm}
+                    setCurrentForm={setCurrentForm}
+                />
+            ) : (
+                ""
+            )}
+        </div>
+    );
 }
 
 export default App;
